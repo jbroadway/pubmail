dc5039733c569a16b0a3b53bfe59babc5b1dcfe1
John Luxford <lux@www.dojolearning.lo>
2009-05-25 02:49:52 -0500

    Initial import

--- settings.php
+++ settings.php
@@ -1,13 +1,13 @@
 ; <?php /* LEAVE THIS LINE FOR SECURITY PURPOSES
 
 ; Sign up for free at messagepub.com to get your API key
-messagepub_api_key = "f5c452c785eb2b5c64e5f25d93631a6edc21de44"
+messagepub_api_key = ""
 
 ; Set this to the admin username for sending emails
-admin_username = "jbroadway"
+admin_username = "default"
 
 ; Set this to the admin password for sending emails
-admin_password = "steaksteak"
+admin_password = "default"
 
 ; This is the default subject line for new emails.
 ; You can change it for each message in the admin area.
@@ -17,6 +17,6 @@ default_subject = "Monthly Newsletter"
 ; isn't abused by rivals and bad guys. Set it to a random
 ; combination of letters and numbers, for example
 ; "sdfg897sdfg789ds678asdf879dfsg87687969sdf".
-unsubscribe_key = "a6df7897df7c89vb789f578xcv6575cvx12"
+unsubscribe_key = ""
 
 ; LEAVE THIS LINE FOR SECURITY PURPOSES */ ?>
