3ec17087e50f283189b8a8338aea8d54c7ffcf26
John Luxford <lux@www.dojolearning.lo>
2009-05-29 14:10:40 -0500

    Added icon, favicon and improved colour scheme

--- html/admin_header.php
+++ html/admin_header.php
@@ -1,9 +1,10 @@
 <html>
 <head>
-	<title>Mailing list admin</title>
+	<title>PubMail - Mailing list admin</title>
 	<link rel="stylesheet" type="text/css" href="../html/admin.css" />
 	<link rel="stylesheet" type="text/css" media="all" href="../html/colorbox/colorbox.css" />
 	<link rel="stylesheet" type="text/css" media="all" href="../html/colorbox/colorbox-custom.css" />
+	<link rel="shortcut icon" href="../html/favicon.ico" />
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 	<script type="text/javascript" src="../html/colorbox/jquery.colorbox-min.js"></script>
 	<script type="text/javascript" src="../html/jquery.pager.js"></script>
@@ -19,6 +20,6 @@
 <body>
 <div id="wrapper">
 
-<h1 id="title">Mailing list admin</h1>
+<h1 id="title">PubMail</h1>
 
 <div id="content">
